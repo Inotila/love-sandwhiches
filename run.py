013d45d57214e4bf0ec9f4f36ef9566645d83e29
Inotila <inoati1991@gmail.com>
2022-01-10 16:50:20 +0000

    set up requirements

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -22,7 +21,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas")
         print("Example:10,20,30,40\n")
 
-        data_str = input("enter your data here: ")
+        data_str = input("enter your data here:\n")
    
         sales_data = data_str.split(",")
     
@@ -88,6 +87,21 @@ def get_last_5_entries_sales():
     
     return columns
 
+def calculate_stock_data(data):
+    """
+    calculating average stock
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
+
 def main():
     """
     Run all program functions
@@ -97,8 +111,9 @@ def main():
     update_worksheet(sales_data,"sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data,"surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to love sandwhiches Data automation")
-#main()
-
-sales_columns = get_last_5_entries_sales()
+main()
