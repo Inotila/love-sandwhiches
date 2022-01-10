b0a0005034cdfe4fca14d217689190e0c5190668
Inotila <inoati1991@gmail.com>
2022-01-10 16:25:00 +0000

    add column collection of sales data

--- run.py
+++ run.py
@@ -74,7 +74,19 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data    
 
+def get_last_5_entries_sales():
+    """
+    collect collumns of data from worksheet
+    """
+    sales = SHEET.worksheet("sales")
+   
 
+    columns=[]
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
 
 def main():
     """
@@ -87,4 +99,6 @@ def main():
     update_worksheet(new_surplus_data,"surplus")
 
 print("Welcome to love sandwhiches Data automation")
-main()
+#main()
+
+sales_columns = get_last_5_entries_sales()
